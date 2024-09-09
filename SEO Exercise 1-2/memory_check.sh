e90a37ec749c9135314167e2f22362d66a7b7de7
itsalva <itsalva@up.edu.ph>
2024-09-09 11:56:35 +0800

    Create memory check script

---
+++ SEO Exercise 1-2/memory_check.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $2 }' )
+TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $3 }' )
+
+while getopts c:w:e: arg; do
+    case $arg in
+        c) ca=$OPTARG;;
+        w) wa=$OPTARG;;
+        e) ea=$OPTARG;;
+        /?) echo "Required parameters are -c for critical level in percentage, -w for warning level in percentage, and -e for email address to send warnings to."
+    esac
+done
+
+if [ ! "$ca" ] || [ ! "$wa" ] || [ ! "$ea" ] ; then
+    echo "Must provide parameters for c, w, and e"
+    exit 1
+fi
+
+if [ $wa -ge $ca ]; then
+    echo "(c)ritical threshold must be higher than (w)arning threshold."
+    exit 1
+fi
+
+critical=$(awk "BEGIN {print $ca / 100}")
+warning=$(awk "BEGIN {print $wa / 100}")
+
+CRITICAL_MEMORY=$(awk "BEGIN {print $TOTAL_MEMORY * $critical}")
+WARNING_MEMORY=$(awk "BEGIN {print $TOTAL_MEMORY * $warning}")
+
+if [ $CURRENT_MEMORY -gt $CRITICAL_MEMORY ]; then
+    echo "Memory is above critical threshold."
+    exit 2
+fi
+
+if [ $CURRENT_MEMORY -ge $WARNING_MEMORY ]; then
+    echo "Memory is above warning threshold."
+    exit 1
+fi
+
+if [ $CURRENT_MEMORY -lt $WARNING_MEMORY ]; then
+    echo "Memory is below warning threshold."
+    exit 0
+fi
